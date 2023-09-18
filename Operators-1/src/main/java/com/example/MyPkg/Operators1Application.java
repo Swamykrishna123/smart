c2c7956be9a47fb228cbc8f5e5d797c47c97432c
Swamykrishna123 <talariswamykrishna@gmail.com>
2023-09-18 10:15:26 +0530

    commit-2

---
+++ Operators-1/src/main/java/com/example/MyPkg/Operators1Application.java
@@ -0,0 +1,13 @@
+package com.example.MyPkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Operators1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Operators1Application.class, args);
+	}
+
+}
---
+++ Operators-1/src/test/java/com/example/MyPkg/Operators1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyPkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Operators1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
